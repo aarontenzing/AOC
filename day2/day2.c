2499a329ac59e178e9028a43c5392e4729faf525
aarontenzing <aarontenzing@gmail.com>
2022-12-07 14:31:23 +0100

    fixed less code

--- day2/day2.c
+++ day2/day2.c
@@ -1,106 +1,52 @@
 #include <stdio.h>
-#include <string.h>
-#define MAX 25000
+#include <stdlib.h>
+#define MAX 4096
 
-int score=0;
+char outcomes1[9][3] = {    
+    {'A','X',4},
+    {'A','Y',8},
+    {'A','Z',3},
+    {'B','X',1},
+    {'B','Y',5},
+    {'B','Z',9},
+    {'C','X',7},
+    {'C','Y',2},
+    {'C','Z',6},
+};
 
-typedef struct players {
-        char p1;
-        char p2;
-    } players;
+char outcomes2[9][3] = {
+    {'A','X',3},
+    {'A','Y',4},
+    {'A','Z',8},
+    {'B','X',1},
+    {'B','Y',5},
+    {'B','Z',9},
+    {'C','X',2},
+    {'C','Y',6},
+    {'C','Z',7},
+};
 
-int switch_case(char input)
-{
-    switch(input) {
-            case 'X':
-                return 1;
-            case 'Y':
-                return 2;
-            case 'Z':
-                return 3;
-            case 'A':
-                return 1; 
-            case 'B':
-                return 2; 
-            case 'C':
-                return 3; 
-        }
-}
-
-int outcome1(int x1,int x2) {
-
-    if( ((x2 == 1) && (x1 == 3)) || ((x2 == 2) && (x1 == 1)) || ((x2==3) && (x1==2)) )
-    {
-        return 1;
-    }
-    else if ( ( (x2 == 3) && (x1 == 1))|| ( (x2 == 2) && (x1 == 3)) || ( (x2 == 1) && (x1 == 2)) )
-    {
-        return 2;
-    }
-    else {
-        return 3;
-    }
-    
-}
-
-int outcome2(int x1,int x2) {
-
-    if( ((x2 == 1) && (x1 == 3)) || ((x2 == 2) && (x1 == 1)) || ((x2==3) && (x1==2)) )
-    {
-        return 1;
-    }
-    else if ( ( (x2 == 3) && (x1 == 1))|| ( (x2 == 2) && (x1 == 3)) || ( (x2 == 1) && (x1 == 2)) )
-    {
-        return 2;
-    }
-    else {
-        return 3;
-    }
-    
-}
-
-int main()
+int main(void)
 {  
-    int x1,x2;
-
     FILE* file = fopen("input.txt","r");
-    if(file == NULL)
+    char buffer[MAX], p1, p2; 
+    int score1=0, score2=0;
+
+    while(fgets(buffer,MAX,file))
     {
-        printf("no such file.\n");
-        return 0;
+        sscanf(buffer,"%c %c", &p1, &p2);
+        for(int i=0; i<9; i++)
+        {
+            if(outcomes1[i][0] == p1 && outcomes1[i][1] == p2)
+            {
+                score1 += (int)(outcomes1[i][2]);
+            }
+            if(outcomes2[i][0] == p1 && outcomes1[i][1] == p2)
+            {
+                score2 += (int)(outcomes2[i][2]);
+            }
+        }
     }
-
-    char buffer[MAX];
-    int res;
-    fgets(buffer,MAX,file);
-
-    while(!feof(file))
-    {
-        players p;
-        sscanf(buffer,"%c %c", &p.p1, &p.p2);
-        fgets(buffer,200,file);
-
-        x1 = switch_case(p.p1);
-        x2 = switch_case(p.p2);
-        res = outcome(x1,x2);
-        printf("res=%d\n",res);
-        
-        if(res == 1)
-        {
-            score += (x2 + 6);
-        }
-        else if(res == 3)
-        {
-            score += (x2 + 3);
-        }
-        else if(res == 2)
-        {
-            score += x2;
-        }
-
-        printf("%c= %d ,%c= %d \n",p.p1,x1,p.p2,x2);
-    }
-    fclose(file);
-    printf("%d \n",score);
-    return 0;
+    printf("score1= %d, score2= %d",score1,score2);
 }
+
