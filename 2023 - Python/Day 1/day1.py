a72ce098efd3eed5465af35e6fb7667f78025c59
aarontenzing <aarontenzing@gmail.com>
2023-12-04 13:05:35 +0100

    day1 finished

--- 2023 - Python/Day 1/day1.py
+++ 2023 - Python/Day 1/day1.py
@@ -1,33 +1,42 @@
 import re
 
-file = open('input.txt', 'r')
+file = open('Day 1\input.txt', 'r')
 lines = file.readlines()
 
-letter_values = {'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9'}
+letter_values = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
 
 total = 0
 
-# puzzle 1
+def replace_numbers(text):
+    replacements = {
+        'one': 'o1e',
+        'two': 't2o',
+        'three': 't3ree',
+        'four': 'f4ur',
+        'five': 'f5ve',
+        'six': 's6x',
+        'seven': 's7ven',
+        'eight': 'e8ght',
+        'nine': 'n9ne'
+    }
+    for word, replacement in replacements.items():
+        text = text.replace(word, replacement)
+    return text
 
 pattern = r"\d"
+# puzzle 1
 for i in lines:
     res = re.findall(pattern, i)
-    total += int(str(res[0] + res[-1]))
+    total += int(res[0])*10 + int(res[-1])
 
 print(f'sum of all of the calibration values? : {total}')
 total = 0
     
 # puzzle 2
-
-pattern = r'\d|one|two|three|four|five|six|seven|eight|nine'
 for i in lines:
+    i = replace_numbers(i)
     res = re.findall(pattern, i)
-    first, last = res[0], res[-1]
-    if (not first.isnumeric()):
-        first = letter_values[res[0]]
-    if (not last.isnumeric()):
-        last = letter_values[res[-1]]
-    total += int(first + last)
-
+    first, last = letter_values[res[0]], letter_values[res[-1]]
+    total += (first * 10) + last
 
 print(f'sum of all of the calibration values? : {total}')   
